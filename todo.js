3164e6107f33a8e6c71baf5859c0e994b59d4024
Andreas Hohwü-Christensen <andreas.hohwu@gmail.com>
2024-04-05 15:12:37 +0200

    And the last part: checking items off

--- todo.js
+++ todo.js
@@ -16,11 +16,27 @@ function add_element(){
     buttonDiv.appendChild(removeButton);
     let newNode = document.createElement('div');
     newNode.classList.add('row');
+    newNode.classList.add('active-list-entry');
     newNode.appendChild(textDiv);
     newNode.appendChild(buttonDiv);
     let list = document.getElementById('list');
     list.appendChild(newNode);
     removeButton.addEventListener('click', (_e) => list.removeChild(newNode))
+    textDiv.addEventListener(
+        'click',
+        (_e) =>
+        {
+            if(newNode.classList.contains('active-list-entry'))
+            {
+                newNode.classList.remove('active-list-entry');
+                newNode.classList.add('inactive-list-entry');
+            }
+            else
+            {
+                newNode.classList.remove('inactive-list-entry');
+                newNode.classList.add('active-list-entry');
+            }
+        });
 }
 
 document.getElementById('add').addEventListener('click', add_element);
